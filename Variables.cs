8dc451ec770d1dca6c344d89cdd1094a7962e10c
Nolan <156261325+nolioli-kra@users.noreply.github.com>
2024-01-24 22:44:50 -0700

    some variable practice using a hypothetical godzilla game

--- Variables.cs
+++ Variables.cs
@@ -2,8 +2,32 @@ using System;
 
 public class VariablesProgram
 {
-    public void Main
+    public Monster godzilla;
+    public NuclearEnergy warhead;
+
+    public void Main()
     {
-        
+        godzilla = new Monster();
+        warhead = new NuclearEnergy();
+
+        godzilla.godzillaSpeed += warhead.nukeBoost;
+        Console.WriteLine(godzilla.godzillaName + " is your current Monster.");
+        Console.WriteLine(godzilla.godzillaName + " has " + godzilla.godzillaHP + " Health remaining!");
+        Console.WriteLine(godzilla.godzillaName + "'s current rival is " + godzilla.godzillaRival +"!");
+        Console.WriteLine(godzilla.godzillaName + " ate the " + warhead.nukeName + " and now has " + godzilla.godzillaSpeed + " speed!");
     }
+}
+
+public class Monster
+{
+    public int godzillaHP = 100;
+    public double godzillaSpeed = 15.5;
+    public string godzillaName = "Gojira";
+    public string godzillaRival = "King Gidorah";
+}
+
+public class NuclearEnergy
+{
+    public string nukeName = "Nuclear Warhead";
+    public double nukeBoost = 2.5;
 }
