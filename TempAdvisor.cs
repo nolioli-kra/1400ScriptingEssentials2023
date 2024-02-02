8fd14fb18c43a83e0d5af514018f4cfa51832d2d
Nolan <11054347@uvu.edu>
2024-02-02 01:22:38 -0700

    Annotated TempAdvisor

--- TempAdvisor.cs
+++ TempAdvisor.cs
@@ -1,12 +1,12 @@
 <<<<<<< HEAD
-using System;
+using System; //I'm not really sure why there are two parts of my code on top of each other. I'm assuming it's because of a merge.
 					
 public class TempAdvisor
 {
 	public static void Main()
 	{
 		Console.WriteLine("Please enter the current temperature in Celcius!");
-		int tempCels = Convert.ToInt32((Console.ReadLine()));
+		int tempCels = Convert.ToInt32((Console.ReadLine())); //converts the user input into an integer the code is able toaccess as a number value.
 		if (tempCels >= 30) {
 			Console.WriteLine("Drink some water! Put that Stanley cup to use!");
 		} else {
