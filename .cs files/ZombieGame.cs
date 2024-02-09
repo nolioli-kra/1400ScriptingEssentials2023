30dc76301024ed8f3e4d07d1f5dd3c0c401c4aa2
Nolan <11054347@uvu.edu>
2024-02-08 19:29:01 -0700

    Added a while loop to my zombie game

--- .cs files/ZombieGame.cs
+++ .cs files/ZombieGame.cs
@@ -6,30 +6,37 @@ public class ZombieGame
 	public Monster zombie;
 	public void Main()
 	{
+		//creating the instances of the Weapon and Monster objects before the while loop is executed will allow them to persist and maintain their state throughout the loop.
 		laserGun = new Weapon();
 		zombie = new Monster();
-		Console.WriteLine("The zombie has " + zombie.zombieHealth + " HP!");
-		ZombieTakeDamage();
-		if (zombie.zombieHealth <= 0)
-		{
-			Console.WriteLine("You defeated the Zombie!");
-		}
-		else
-		{
+		while (zombie.zombieAlive == true)
+		{ //while loop continues the game as long as the zombie is alive
 			Console.WriteLine("The zombie has " + zombie.zombieHealth + " HP!");
-			Console.WriteLine(laserGun.weaponName + " has " + laserGun.ammoCount + " ammo remaining.");
+			//Console.WriteLine(laserGun.weaponName + " has " + laserGun.ammoCount + " ammo remaining.");
 			ZombieTakeDamage();
-		}
-	}
+			
+			if (zombie.zombieHealth <= 0)
+			{
+				zombie.zombieAlive = false;
+				Console.WriteLine("You defeated the Zombie!");
+			} // if
+			else
+			{
+				Console.WriteLine("The zombie has " + zombie.zombieHealth + " HP!");
+				ZombieTakeDamage();
+			} //else
+		} //while close
+	} //main
 
 	public void ZombieTakeDamage()
 	{
+		Console.WriteLine(laserGun.weaponName + " has " + laserGun.ammoCount + " ammo remaining.");
 		Console.WriteLine("How many shot's do you want to fire?");
 		int shotsFired = Convert.ToInt32((Console.ReadLine()));
-		zombie.zombieHealth -= Convert.ToInt32(shotsFired * laserGun.weaponStrength);
+		zombie.zombieHealth = Math.Max(0, zombie.zombieHealth - Convert.ToInt32(shotsFired * laserGun.weaponStrength));
 		laserGun.ammoCount -= shotsFired;
-	}
-}
+	} //zombietakedamage
+} //zombiegame
 
 public class Weapon
 {
@@ -41,9 +48,5 @@ public class Weapon
 public class Monster
 {
 	public int zombieHealth = 200;
-}
-
-public class Monster
-{
-	public int zombieHealth = 200;
+	public bool zombieAlive = true;
 }
